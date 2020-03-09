7f4f05ef3dd160e952bb01100be733ee9cd52771
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-03-09 14:55:52 +0100

    Tweak benchmarks and rerun with latest packages

--- benchmarks/lu_decomposition/lu.jl
+++ benchmarks/lu_decomposition/lu.jl
@@ -5,7 +5,7 @@ function det_by_lu(y, x, N)
         y[1] *= x[k,k]
         for i = k+1:N
             x[i,k] /= x[k,k]
-	end
+	    end
         for j = k+1:N
             for i = k+1:N
                 x[i,j] -= x[i,k] * x[k,j]
@@ -14,9 +14,12 @@ function det_by_lu(y, x, N)
     end
 end
 
+nb_loops = []
+
 function run_julia(y,A,B,N)
     loops = max(10000000 // (N*N), 1)
-    print(loops)
+    append!(nb_loops, loops)
+    print(loops, '\n')
     for l in 1:loops
         B[:,:] = A
         det_by_lu(y, B, N)
@@ -25,45 +28,50 @@ end
 
 y = [0.0]
 
+times = []
+
 N=5
 A = rand(N,N)
 B = zeros(N,N)
+# Warm up
 @time run_julia(y,A,B,N)
+
 N=5
 A = rand(N,N)
 B = zeros(N,N)
-@time run_julia(y,A,B,N)
+append!(times, @elapsed run_julia(y,A,B,N))
 N=10
 A = rand(N,N)
 B = zeros(N,N)
-@time run_julia(y,A,B,N)
+append!(times, @elapsed run_julia(y,A,B,N))
 N=30
 A = rand(N,N)
 B = zeros(N,N)
-@time run_julia(y,A,B,N)
+append!(times, @elapsed run_julia(y,A,B,N))
 N=100
 A = rand(N,N)
 B = zeros(N,N)
-@time run_julia(y,A,B,N)
+append!(times, @elapsed run_julia(y,A,B,N))
 N=200
 A = rand(N,N)
 B = zeros(N,N)
-@time run_julia(y,A,B,N)
+append!(times, @elapsed run_julia(y,A,B,N))
 N=300
 A = rand(N,N)
 B = zeros(N,N)
-@time run_julia(y,A,B,N)
+append!(times, @elapsed run_julia(y,A,B,N))
 N=400
 A = rand(N,N)
 B = zeros(N,N)
-@time run_julia(y,A,B,N)
+append!(times, @elapsed run_julia(y,A,B,N))
 N=600
 A = rand(N,N)
 B = zeros(N,N)
-@time run_julia(y,A,B,N)
+append!(times, @elapsed run_julia(y,A,B,N))
 N=1000
 A = rand(N,N)
 B = zeros(N,N)
-@time run_julia(y,A,B,N)
+append!(times, @elapsed run_julia(y,A,B,N))
 
-
+print("\ntime per loop=\n", times ./ nb_loops[2:end])
+# [5, 10, 30, 100, 200, 300, 400, 600, 1000])
